6790ab968d3dada03c991f17a32d93a54b260b79
Julien <julienbourgeois.r@gmail.com>
2022-02-07 15:39:20 +0100

    màj DB

--- backend/database/projet_07_tableSchema.sql
+++ backend/database/projet_07_tableSchema.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS utilisateur;
 
 CREATE TABLE utilisateur (
 user_id INTEGER PRIMARY KEY NOT NULL auto_increment,
-nom VARCHAR(100),
-prenom VARCHAR(100),
-pseudo VARCHAR(100),
-email VARCHAR(255) NOT NULL UNIQUE,
-user_password VARCHAR(500) NOT NULL, 
+nom VARCHAR(25),
+prenom VARCHAR(25),
+pseudo VARCHAR(50),
+email VARCHAR(50) NOT NULL UNIQUE,
+user_password VARCHAR(25) NOT NULL, 
 isAdmin TINYINT(0) NOT NULL 
 );
 
@@ -25,7 +25,7 @@ CREATE TABLE commentaire (
 	comment_id INTEGER PRIMARY KEY NOT NULL auto_increment,
     user_id INTEGER NOT NULL,
     post_id INTEGER NOT NULL,
-	content VARCHAR(5000),
+	content VARCHAR(500),
     FOREIGN KEY(post_id) references publication (post_id) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (user_id) REFERENCES utilisateur (user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
